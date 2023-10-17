562d589bc99855c0abdee7be8b5ec96b786a9b61
Tum8Riew <you@example.com>
2023-10-17 19:35:30 +0700

    add V2.0.0

---
+++ V2/quote_test.go
@@ -0,0 +1,19 @@
+package quote
+
+import "testing"
+
+func TestSay2(t *testing.T) {
+	want := "Hello"
+
+	if got := Say2(); got != want {
+		t.Errorf("Say2() = %q , want : %q", got, want)
+	}
+}
+
+func TestSpeak2(t *testing.T) {
+	want := "Hi, Mate"
+
+	if got := Speak2(); got != want {
+		t.Errorf("Speak2() = %q , want : %q", got, want)
+	}
+}
